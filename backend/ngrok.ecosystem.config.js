595db5faedbf85ed1320f32e4cc4b5c721df0ad0
xdaolincoln <swmhzj533281@hotmail.com>
2025-12-10 09:15:28 +0000

    update

---
+++ backend/ngrok.ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [{
+    name: 'ngrok-all',
+    script: 'ngrok',
+    args: 'start --all --config=/root/.config/ngrok/ngrok.yml --authtoken=2v7WZRHgrhCsjZGZSBi21qpwYSQ_7pW7MBk9CVNtTxD226xMM'
+  }]
+};
+
