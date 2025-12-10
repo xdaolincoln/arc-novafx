6bbc7fa47d3211f7dd03162b1e7c71196d84b0db
xdaolincoln <swmhzj533281@hotmail.com>
2025-12-10 08:46:07 +0000

    update

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
-      PORT: 3001
+      PORT: 3002
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
